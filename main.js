2c30a52c98b249ff3a86e7b6ca8b1d3f17152b31
Faizan <81250304+Faizanishfaqahmed@users.noreply.github.com>
2024-04-22 23:45:06 +0500

    Update main.js

--- main.js
+++ main.js
@@ -8,3 +8,57 @@ backDelay : 1000,
 loop:true
 });
 
+
+
+function press(){
+    let drop = document.querySelector(".dropdown");
+    let menu = document.querySelector(".menue");
+      
+    drop.classList.toggle("active");
+    menu.classList.toggle("active");
+
+    menu.style.background = 'cyan' ;
+}
+
+document.onclick = function(e){
+    let drop = document.querySelector(".dropdown");
+    let menu = document.querySelector(".menue");
+    
+    if(!menu.contains(e.target)&& !drop.contains(e.target)){
+        drop.classList.remove("active");
+        menu.classList.remove ("active");
+      
+    }
+}
+
+function aboutClick(){
+    let aboutBtn = document.querySelector(".details");
+    let aboutText = document.querySelector(".about-p");
+    aboutText.classList.toggle("p-active");
+}
+ 
+var serBtn = document.querySelectorAll(".ser-btn");
+    var textCard = document.querySelectorAll(".card-text");
+function serviceBtn1(){
+    
+    serBtn[0].classList.toggle("ser-btnactive");
+    textCard[0].classList.toggle("cardText-active");
+
+    
+}
+function serviceBtn2(){
+    
+    serBtn[1].classList.toggle("ser-btnactive");
+    textCard[1].classList.toggle("cardText-active");
+
+    
+}
+function serviceBtn3(){
+    
+    serBtn[2].classList.toggle("ser-btnactive");
+    textCard[2].classList.toggle("cardText-active");
+
+    
+}
+
+ 
